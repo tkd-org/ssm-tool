a72472d696528f6f0cdb5c63aa41362eecc9fbaf
tokuda <akihisa.tkd@gmail.com>
2024-01-30 10:28:46 +0900

    fix

--- access/scp.sh
+++ access/scp.sh
@@ -1,8 +1,8 @@
-export AWS_PROFILE=$1
+file=$1
 user_name=ec2-user
-
 # Path: access/login.sh
 target=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=*-bation" | jq -r '[.Reservations[].Instances[] | .InstanceId] | join(" ")')
 echo $target
 
-ssh_ec2 ${user_name} $target
+ssh_ec2 ${user_name} $target --send-key-only
+scp $file ec2-user@$target://home/${user_name}/
