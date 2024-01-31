c84bd58b39782632686d8aedac2c8dc14082dd31
tokuda <akihisa.tkd@gmail.com>
2024-01-31 18:12:42 +0900

    filter running instance

--- access/login.sh
+++ access/login.sh
@@ -2,7 +2,7 @@ export AWS_PROFILE=$1
 user_name=ec2-user
 
 # Path: access/login.sh
-target=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=*-bation" | jq -r '[.Reservations[].Instances[] | .InstanceId] | join(" ")')
+target=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=*-bation" | jq -r '[.Reservations[].Instances[] |select(.State.Name == "running")|.InstanceId] | join(" ")')
 echo $target
 
 ssh_ec2 ${user_name} $target
