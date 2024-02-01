d41b8012bc455527a402d42fd9e98cc4ae89f823
tokuda <akihisa.tkd@gmail.com>
2024-02-01 20:51:00 +0900

    fix

--- access/scp.sh
+++ access/scp.sh
@@ -1,7 +1,8 @@
 file=$1
 user_name=ec2-user
 # Path: access/login.sh
-target=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=*-bation" | jq -r '[.Reservations[].Instances[] | .InstanceId] | join(" ")')
+target=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=*-bation" | jq -r '[.Reservations[].Instances[] |select(.State.Name == "running")|.InstanceId] | join(" ")')
+
 echo $target
 
 ssh_ec2 ${user_name} $target --send-key-only
